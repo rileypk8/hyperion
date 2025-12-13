acbc0a816797109589b26ec716c7a331fd0ce248
rileypk8 <rileypk8@gmail.com>
2025-12-13 09:27:23 -0500

    Add hub-and-spoke landing page with studio-specific visualizations
    - Create LandingPage with central Disney castle linking to company-wide analytics
    - Add studio nodes arranged radially around the hub (WDAS, Pixar, KH, etc.)
    - Create KingdomHeartsHub with character relationship network graph using react-force-graph-2d
    - Create WDASHub with soundtrack timeline and Oscar visualizations
    - Create GenericStudioHub for other studios (Pixar, Blue Sky, DisneyToon, etc.)
    - Update routing: / is now hub landing, /dashboard is analytics
    - Update navigation to include Hub and Analytics links

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { MainLayout } from './layouts/MainLayout';
 import {
+  LandingPage,
   Dashboard,
   StudioList,
   StudioDetail,
@@ -9,6 +10,9 @@ import {
   FilmDetail,
   CharacterList,
   CharacterDetail,
+  KingdomHeartsHub,
+  WDASHub,
+  GenericStudioHub,
 } from './pages';
 import './App.css';
 
@@ -18,7 +22,13 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<MainLayout />}>
-            <Route index element={<Dashboard />} />
+            <Route index element={<LandingPage />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            {/* Studio Hubs */}
+            <Route path="hub/kingdom-hearts" element={<KingdomHeartsHub />} />
+            <Route path="hub/wdas" element={<WDASHub />} />
+            <Route path="hub/:studioId" element={<GenericStudioHub />} />
+            {/* Data browsing */}
             <Route path="studios" element={<StudioList />} />
             <Route path="studio/:id" element={<StudioDetail />} />
             <Route path="films" element={<FilmList />} />
